47e3900a4838401ec2fb538c6f9d76130c7b61c0
adedotunolamide01 <adedotuntoyese1@gmail.com>
2022-03-10 23:48:38 +0100

    My 4 task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+#include < stadio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 after priniting the function
+ */
+int main(void)
+{
+put("\"Programming is like build a multilingual puzzle");
+return (0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+[A[A[A[A[A[A[A[A[A[A[A[A[A}
